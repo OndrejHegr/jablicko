afef966c9508e7601e4957941bd195ba67dfb223
Ondrej Hegr <ondrej.hegr@gmail.com>
2024-12-27 13:56:46 +0100

    testovaci data obsahuji formatovani

--- publish.php
+++ publish.php
@@ -30,27 +30,123 @@ function mqtt_publish($mqtt_topic, $mqtt_data): void
 }
 
 const COLOR_WHITE = '#ffffff';
+const COLOR_BLACK = '#000000';
 
 $competitionId = 1;
 
 $mqtt_topic = 'contest/'.$competitionId;
 
-$data = [
-    //'published' => date('Y-m-d H:i:s'),
-    'performance_title' => ['left' => 50, 'top' => 50, 'width' => 700, 'height' => 40, 'value' => 'Easy on Me', 'color' => '#dddd00', 'family' => 'serif', 'size' => 30, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    //'performance_title' => ['left' => 50, 'top' => 50, 'width' => 700, 'height' => 40, 'value' => 'Hello', 'color' => '#dddd00', 'family' => 'serif', 'size' => 30, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    //'performance_title' => ['left' => 50, 'top' => 50, 'width' => 700, 'height' => 40, 'value' => 'Rolling Deep', 'color' => '#dddd00', 'family' => 'serif', 'size' => 30, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    //'performance_title' => ['left' => 50, 'top' => 50, 'width' => 700, 'height' => 40, 'value' => 'Set Fire to the Rain', 'color' => '#dddd00', 'family' => 'serif', 'size' => 30, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    //'performance_title' => ['left' => 50, 'top' => 50, 'width' => 700, 'height' => 40, 'value' => 'Someone like You', 'color' => '#dddd00', 'family' => 'serif', 'size' => 30, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    'club_title' => ['left' => 50, 'top' => 90, 'width' => 700, 'height' => 40, 'value' => 'KTS Příbram', 'color' => '#dddd00', 'family' => 'serif', 'size' => 30, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    'event_title' => ['left' => 50, 'top' => 130, 'width' => 700, 'height' => 40, 'value' => '1. Freestyle (Semifinále)', 'color' => '#dddd00', 'family' => 'serif', 'size' => 25, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    'contest_name' => ['left' => 50, 'top' => 900, 'width' => 700, 'height' => 50, 'value' => 'Memoriál Bohuslava Matiáše', 'color' => '#dddd00', 'family' => 'serif', 'size' => 50, 'style' => 'normal', 'align' => 'left', 'border' => 0],
-    'venue_place' => ['left' => 50, 'top' => 950, 'width' => 700, 'height' => 20, 'value' => 'Kolín', 'color' => '#dddd00', 'family' => 'serif', 'size' => 35, 'style' => 'normal', 'align' => 'left', 'border' => 0],
+$clubs = [
+    'KTS Příbram',
+    'TK Astra Praha',
+    'TK Bonstep Hradec Králové',
+    'TK Akcent Ostrava'
 ];
-/*
- *
 
-*/
+$performances = [
+    'Set Fire to the Rain',
+    'Skyfall',
+    'Hello',
+    'Someone Like You',
+    'Love In The Dark',
+    'Easy on Me',
+    'Rolling in the Deep',
+    'When We Were Young',
+    'Lovesong'
+];
+
+$choreographers = [
+    'Dominik Vodička',
+    'Petr Čadek',
+    'Oskar Hes'
+];
+
+$ci = rand(0, count($clubs) - 1);
+$pi = rand(0, count($performances) - 1);
+$chi = rand(0, count($choreographers) - 1);
+
+$data = [
+    'performance_title' => [
+        'x' => 50,
+        'y' => 580,
+        'w' => 900,
+        'h' => 40,
+        'family' => 'cursive',
+        'size' => 30,
+        'style' => 'normal',
+        'weight' => 'normal',
+        'align' => 'left',
+        'color' => COLOR_BLACK,
+        'border' => 0,
+        'value' => $performances[$pi]
+    ], 'contest_name' => [
+        'x' => 50,
+        'y' => 50,
+        'w' => 900,
+        'h' => 60,
+        'family' => 'sans-serif',
+        'size' => 50,
+        'style' => 'normal',
+        'weight' => 'bold',
+        'align' => 'center',
+        'color' => COLOR_BLACK,
+        'border' => 0,
+        'value' => 'Memoriál Bohuslava Matiáše'
+    ], 'venue_place' => [
+        'x' => 50,
+        'y' => 110,
+        'w' => 900,
+        'h' => 35,
+        'family' => 'serif',
+        'size' => 35,
+        'style' => 'italic',
+        'weight' => 'normal',
+        'align' => 'center',
+        'color' => '#00ee00',
+        'border' => 0,
+        'value' => 'Kolín'
+    ], 'event_title' => [
+        'x' => 50,
+        'y' => 550,
+        'w' => 900,
+        'h' => 30,
+        'family' => 'monospace',
+        'size' => 25,
+        'style' => 'italic',
+        'weight' => 'normal',
+        'align' => 'left',
+        'color' => '#ee0000',
+        'border' => 0,
+        'value' => '1. Volný tanec'
+    ], 'club_title' => [
+        'x' => 50,
+        'y' => 620,
+        'w' => 900,
+        'h' => 35,
+        'family' => 'sans-serif',
+        'size' => 35,
+        'style' => 'normal',
+        'weight' => 'bold',
+        'align' => 'left',
+        'color' => COLOR_BLACK,
+        'border' => 0,
+        'value' => $clubs[$ci]
+    ], 'choreographer' => [
+        'x' => 50,
+        'y' => 655,
+        'w' => 900,
+        'h' => 35,
+        'family' => 'sans-serif',
+        'size' => 35,
+        'style' => 'normal',
+        'weight' => 'bold',
+        'align' => 'right',
+        'color' => '#0000ff',
+        'border' => 0,
+        'value' => $choreographers[$chi]
+    ]
+];
+
 $mqtt_data = json_encode($data);
 mqtt_publish($mqtt_topic, $mqtt_data);
 
